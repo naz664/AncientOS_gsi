d12c7998882ae2be1abcf3d92c7a60974c4eb1bf
naz664 <nazimnavas145@gmail.com>
2022-10-22 08:51:27 +0530

    Changes for Oct2022

---
+++ Ancient.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+$(call inherit-product, vendor/ancient/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/ancient/config/BoardConfig.mk)
+$(call inherit-product, device/ancient/sepolicy/common/sepolicy.mk)
+-include vendor/ancient/build/core/config.mk
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+#ANCIENT_GAPPS=true
+#FORCE_NEXUSLAUNCHER=true
+#ANCIENT_WEEABO=true
+ANCIENT_OFFICIAL := true
---
+++ README.md
@@ -0,0 +1,13 @@
+# AncientOS_patches
+Patches for building AncientOS gsi
+
+## Credits
+These people have helped this project in some way or another, so they should be the ones who receive all the credit:
+- [AncientOS Team](https://github.com/Ancient-Roms)
+- [Phhusson](https://github.com/phhusson)
+- [AndyYan](https://github.com/AndyCGYan)
+- [Ponces](https://github.com/ponces)
+- [ChonDoit](https://github.com/ChonDoit)
+
+# Website
+- [Link](https://www.ancientrom.xyz/download_list.php?brand_names=GSI)
